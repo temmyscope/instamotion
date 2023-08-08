5254f026f737edb024f6204add2f78f415fb5e74
Elisha OYAWALE <temmyscope@Elishas-Air.fritz.box>
2023-08-08 21:48:32 +0200

    Improve data and adapter layer

--- app/(api)/services/vehicle.ts
+++ app/(api)/services/vehicle.ts
@@ -3,19 +3,39 @@ import { requests } from "@/app/(api)/api";
 import { GetVehicles, Vehichle } from "@/app/lib/types";
 
 
-const vehicleAdapter = (data: any): Vehichle => {
+const vehicleFeatureAdapter = (data: any): Vehichle => {
+
+  let [month, year] = (data.vehicle_history.reg_date as string).split('.', 2);
 
   return ({
     id: data.id,
-    co2: data.id,
-    make: data.id,
-    fuel: data.id,
-    model: data.id,
-    power: data.id,
-    images: [data.id],
-    mileage: data.id,
-    consumption: data.id,
-    first_registration: data.id,
+    co2: {
+      class: data.environment.pollutant,
+      emission: data.environment.emission
+    },
+    color: data.color,
+    make: data.brand,
+    fuel: data.drivetrain.fuel.type,
+    model: {
+      model: data.model,
+      detail: data.model_text_extended,
+      year: data.model_year
+    },
+    power: data.performance,
+    price: { 
+      price: data.price_data.price, 
+      currency: data.price_data.currency 
+    },
+    images: data.media.final,
+    mileage: data.kilometer,
+    consumption: {
+      unit: data.drivetrain.consumption.unit,
+      combined:  data.drivetrain.consumption.consumption_combined,
+    },
+    first_registration: {
+      month: Number(month),
+      year: Number(year),
+    },
   });
 }
 
@@ -36,7 +56,7 @@ export const getVehichles: GetVehicles = async() => {
     data => {
       if (data['records'] && data['records']?.length > 0) {
         console.log(data['records'])
-        return (data['records'] as Array<any>).map(datum => vehicleAdapter(datum));
+        return (data['records'] as Array<any>).map(vehicle => vehicleFeatureAdapter(vehicle));
       }
       return [];
     }
--- app/lib/types.ts
+++ app/lib/types.ts
@@ -7,13 +7,31 @@ export interface Vehichle{
   id: string
   images: Array<string>
   make: string
-  model: string
+  model: {
+    model: string
+    detail: string
+    year: number
+  }
   mileage: number
-  first_registration: number
+  first_registration: {
+    month: number
+    year: number
+  }
   fuel: string
   power: string
-  consumption: number
-  co2: string
+  price: { 
+    price: number
+    currency: string
+  }
+  consumption: {
+    unit: string
+    combined:  number|null
+  }
+  co2: {
+    class: string
+    emission: number|null
+  }
+  color: string
 }
 
 export type GetVehicles = () => Promise<Array<Vehichle>>
