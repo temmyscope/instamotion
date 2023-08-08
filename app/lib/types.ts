c7212976806e076e832503afb151858659fa2eba
Elisha OYAWALE <temmyscope@Elishas-Air.fritz.box>
2023-08-08 16:59:23 +0200

    Initialise folder structure

---
+++ app/lib/types.ts
@@ -0,0 +1,12 @@
+export interface Vehichle {
+  id: string
+  image: string
+  make: string
+  model: string
+  mileage: number
+  first_registration: number
+  fuel: string
+  power: string
+  consumption: number
+  co2: string
+}
