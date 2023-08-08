0e3a4414ac38adffbef4584d36cf372cebbba3d2
Elisha OYAWALE <temmyscope@Elishas-Air.fritz.box>
2023-08-08 22:35:34 +0200

    Add NextJS configuration for image host

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'dqg4c1i67ln7w.cloudfront.net',
+        port: '',
+        pathname: '/photos/edited/**',
+      },
+    ],
+  },
+}
 
 module.exports = nextConfig
