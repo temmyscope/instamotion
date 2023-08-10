46afd73da75d8ed2b2074ffa64f8571ae948dd68
Elisha OYAWALE <temmyscope@Elishas-Air.fritz.box>
2023-08-10 18:30:47 +0200

    Add metadata extractor

--- app/(api)/services/vehicle.ts
+++ app/(api)/services/vehicle.ts
@@ -1,8 +1,12 @@
 import { requests } from "@/app/(api)/api";
-import { GetVehicles, Vehichle } from "@/app/lib/types";
+import { GetVehicles, Vehicle, VehicleMetaDataType } from "@/app/lib/types";
 
-
-const vehicleFeatureAdapter = (data: any): Vehichle => {
+/**
+ * modifies retrieved items into useable item
+ * @param data 
+ * @returns {Vehicle}
+ */
+const vehicleFeatureAdapter = (data: any): Vehicle => {
   let month = null, year = null;
 
   if (data.vehicle_history.reg_date) {
@@ -18,6 +22,7 @@ const vehicleFeatureAdapter = (data: any): Vehichle => {
       emission: data.environment.emission
     },
     color: data.color,
+    gearbox: data.drivetrain?.transmission_type ?? '',
     make: data.brand,
     fuel: data.drivetrain.fuel.type,
     model: {
@@ -30,6 +35,7 @@ const vehicleFeatureAdapter = (data: any): Vehichle => {
       price: data.price_data.price, 
       currency: data.price_data.currency 
     },
+    category: data.category,
     images: data.media.final,
     mileage: data.kilometer,
     consumption: {
@@ -51,21 +57,50 @@ export const getVehichles: GetVehicles = async() => {
 
   const api = requests(
     [], 
-    [ErrorResponseInterceptor], 'https://run.mocky.io/v3/e7d5a5aa-8bdf-4a36-b6ab-134c08df916b'
+    [ErrorResponseInterceptor], 
+    'https://run.mocky.io/v3/e7d5a5aa-8bdf-4a36-b6ab-134c08df916b'
   );
 
   return api.get('/').then(
     data => {
       if (data['records'] && data['records']?.length > 0) {
-        let vehicleData: Array<Vehichle> = [];
-        for (let index = 0; index < 30; index++) {
-          vehicleData = [
-            ...vehicleData, ...(data['records'] as Array<any>).map(vehicle => vehicleFeatureAdapter(vehicle))
-          ];
+        let vehicleData: Array<Vehicle> = [];
+        let VehicleMetaData: VehicleMetaDataType = {
+           'make': {} as {[key: string]: Set<string>}, 'first_reg': new Set<number>(),
+          'color': new Set(), 'fuel': new Set(), 'gearbox': new Set(), 'categories': new Set(),
+        };
+
+        let formattedData = (data['records'] as Array<any>).map(
+          vehicle => { 
+            let vf = vehicleFeatureAdapter(vehicle);
+            VehicleMetaData = getVehichleMetaData(vf, VehicleMetaData);
+            return vf;
+          }
+        );
+        // increase number of retrieved items by iteratively incrementing it
+        for (let index = 0; index < 30; index++) {  
+          vehicleData = [ ...vehicleData, ...formattedData ];
         }
-        return vehicleData;
+        // console.log(data['records']);
+        return { vehicles: vehicleData, meta: VehicleMetaData};
       }
-      return [];
+      return { vehicles: [], meta: {} as VehicleMetaDataType };
     }
   );
+}
+
+
+export const getVehichleMetaData = (vf: Vehicle, VehicleMetaData: VehicleMetaDataType) => {
+  VehicleMetaData['categories'].add(vf.category);
+  VehicleMetaData['make'][vf.make] = VehicleMetaData['make'][vf.make]?.add(
+    vf.model.model
+  ) ?? new Set([vf.model.model]);
+  VehicleMetaData['fuel'] = VehicleMetaData['fuel'].add(vf.fuel);
+  VehicleMetaData['color'] = VehicleMetaData['color'].add(vf.color);
+  if (vf.first_registration.year) {
+    VehicleMetaData['first_reg'] = VehicleMetaData['first_reg'].add(vf.first_registration.year);
+  }
+  VehicleMetaData['gearbox'] = VehicleMetaData['gearbox'].add(vf.gearbox);
+
+  return VehicleMetaData;
 }
