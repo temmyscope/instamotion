c44f41616c6244c36b1597b8ec846dd7af87e7ce
Elisha OYAWALE <temmyscope@Elishas-Air.fritz.box>
2023-08-13 22:42:00 +0200

    Add hover effect on car labels

--- app/components/commons/label/index.tsx
+++ app/components/commons/label/index.tsx
@@ -1,15 +1,22 @@
+import { useState } from "react";
+
 export default function Label({ name, label, id }: { name: string, label: string, id: string }) {
+  const [isVisibleTooltip, setVisiblity] = useState(false);
   
   return (
-  <div style={{cursor: "pointer"}}>
+  <div 
+    style={{cursor: "pointer"}} 
+    onMouseEnter={() => setVisiblity(true)} 
+    onMouseLeave={() => setVisiblity(false)}
+  >
     <span 
-      data-tooltip-target={`tooltip-${id}`} data-tooltip-style="dark" data-tooltip-placement="top"
       className="bg-gray-100 text-gray-800 text-xs my-1 font-small ml-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">
     {label}
     </span>
-    <div id={`tooltip-${id}`} role="tooltip" className="absolute z-10 invisible inline-block px-2 py-1 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-md shadow-sm opacity-0 tooltip">
+    <div role="tooltip" 
+      className={`absolute ${!isVisibleTooltip && 'invisible'} inline-block px-2 py-1 text-sm font-medium text-gray-900 bg-white border border-gray-300 rounded-md`}
+    >
       {name}
-      <div className="tooltip-arrow" data-popper-arrow></div>
     </div>
   </div>
   );
